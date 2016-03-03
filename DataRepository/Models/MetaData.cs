7fbc56b7a21781a2d1f648f2c5a1946a314b1685
gray pritchett <gray.pritchett@creativeintersection.com.au>
2016-03-03 17:26:12 +1000

    regattacrew

---
+++ DataRepository/Models/MetaData.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace DataRepository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public  class RegattaMetaData
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Regatta name cannot be empty")]
+        [Display(Description = "Regatta Name")]
+        [StringLength(255, MinimumLength = 4, ErrorMessage = "Regatta Name must be more than {2} and less than {1} characters")]
+        public string RegattaName { get; set; }
+        [Range(typeof(DateTime), "1 Jan 2000", "31 Dec 2050", ErrorMessage = "Date must be between {1} and {2}.")]
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<int> RegattaYear { get; set; }
+        public string Result { get; set; }
+    }
+
+    public class CrewMemberMetaData
+    {
+        [Required(ErrorMessage="Crew Name cannot be empty")]
+        public string CrewName { get; set; }
+        [EmailAddress]
+        public string CrewEmail { get; set; }   
+    }
+}
