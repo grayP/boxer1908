154e100f4b989489ef9c81c399261604a0313bf3
Gray Pritchett <gray.pritchett@smsmt.com>
2017-05-02 11:58:17 +1000

    Fix dates and update references

--- bw01/App_Start/IdentityConfig.cs
+++ bw01/App_Start/IdentityConfig.cs
@@ -54,8 +54,8 @@ namespace bw01
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
                 RequireUppercase = true,
             };
